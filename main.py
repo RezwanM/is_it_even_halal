35ade1ccfede4d6e0742982fd9b48a1dd1d70505
Rezwan Matin <rezwanmatin@gmail.com>
2025-06-18 14:11:14 -0400

    Reformatted scripts

--- main.py
+++ main.py
@@ -1,37 +1,61 @@
 import json
 import os
 import requests
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
 
 
 load_dotenv()
-api_key=os.environ.get("USDA_API_KEY")
+api_key = os.environ.get("USDA_API_KEY")
 url = "https://api.nal.usda.gov/fdc/v1/foods/search"
-ingredients = set() 
-haram_list = set() 
-animal_derived_ingredients = ["enzymes", "whey", "lard", "bacon", "ham", "gelatin", "rennet", "l-cysteine", "pepsin"]
-alcohol_ingredients = ["alcohol", "ethanol", "vanilla extract", "wine vinegar", "malt extract"] 
-additives_ingredients = ["monoglycerides", "diglycerides", "glycerin", "glycerol", "shortening", "magensium stearate"]  
+ingredients = set()
+haram_list = set()
+animal_derived_ingredients = [
+    "enzymes",
+    "whey",
+    "lard",
+    "bacon",
+    "ham",
+    "gelatin",
+    "rennet",
+    "l-cysteine",
+    "pepsin",
+]
+alcohol_ingredients = [
+    "alcohol",
+    "ethanol",
+    "vanilla extract",
+    "wine vinegar",
+    "malt extract",
+]
+additives_ingredients = [
+    "monoglycerides",
+    "diglycerides",
+    "glycerin",
+    "glycerol",
+    "shortening",
+    "magensium stearate",
+]
 insects_derived_ingredients = ["carmine (e120)"]
-haram_ingredients = animal_derived_ingredients + alcohol_ingredients + additives_ingredients + insects_derived_ingredients 
+haram_ingredients = (
+    animal_derived_ingredients
+    + alcohol_ingredients
+    + additives_ingredients
+    + insects_derived_ingredients
+)
 query = input("Enter the item name: ")
-params = {
-    "query": query,
-    "dataType": ["Branded"],
-    "api_key": api_key
-}
+params = {"query": query, "dataType": ["Branded"], "api_key": api_key}
 response = requests.get(url, params=params)
 if response.status_code == 200:
     with open("result.json", "a", encoding="utf-8") as f:
         json.dump(response.json(), f, indent=4)
     for item in response.json()["foods"]:
-        ingredients_list = item["ingredients"].split(',')
+        ingredients_list = item["ingredients"].split(",")
         for ingredient in ingredients_list:
             ingredients.add(ingredient.strip().lower())
             if ingredient.strip().lower() in haram_ingredients:
                 haram_list.add(ingredient.strip().lower())
     if haram_list:
-        print("This item might not be halal as it contains:") 
+        print("This item might not be halal as it contains:")
         print(haram_list)
     else:
         print("This item is halal.")
--- webapp.py
+++ webapp.py
@@ -1,33 +1,58 @@
 import json
 import os
 import requests
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
 from flask import Flask, redirect, url_for, request
 
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["POST", "GET"])
 def check_halal():
     load_dotenv()
-    api_key=os.environ.get("USDA_API_KEY")
+    api_key = os.environ.get("USDA_API_KEY")
     url = "https://api.nal.usda.gov/fdc/v1/foods/search"
-    ingredients = set() 
-    haram_list = set() 
-    animal_derived_ingredients = ["enzymes", "whey", "lard", "bacon", "ham", "gelatin", "rennet", "l-cysteine", "pepsin"]
-    alcohol_ingredients = ["alcohol", "ethanol", "vanilla extract", "wine vinegar", "malt extract"] 
-    additives_ingredients = ["monoglycerides", "diglycerides", "glycerin", "glycerol", "shortening", "magensium stearate"]  
+    ingredients = set()
+    haram_list = set()
+    animal_derived_ingredients = [
+        "enzymes",
+        "whey",
+        "lard",
+        "bacon",
+        "ham",
+        "gelatin",
+        "rennet",
+        "l-cysteine",
+        "pepsin",
+    ]
+    alcohol_ingredients = [
+        "alcohol",
+        "ethanol",
+        "vanilla extract",
+        "wine vinegar",
+        "malt extract",
+    ]
+    additives_ingredients = [
+        "monoglycerides",
+        "diglycerides",
+        "glycerin",
+        "glycerol",
+        "shortening",
+        "magensium stearate",
+    ]
     insects_derived_ingredients = ["carmine (e120)"]
-    haram_ingredients = animal_derived_ingredients + alcohol_ingredients + additives_ingredients + insects_derived_ingredients 
-    if request.method == 'POST':
-        query = request.form["nm"] 
+    haram_ingredients = (
+        animal_derived_ingredients
+        + alcohol_ingredients
+        + additives_ingredients
+        + insects_derived_ingredients
+    )
+    if request.method == "POST":
+        query = request.form["nm"]
     else:
         query = request.args.get("nm")
-    params = {
-        "query": query,
-        "dataType": ["Branded"],
-        "api_key": api_key
-    }
+    params = {"query": query, "dataType": ["Branded"], "api_key": api_key}
     response = requests.get(url, params=params)
     if response.status_code == 200:
         for item in response.json()["foods"]:
@@ -43,12 +68,14 @@ def check_halal():
             message = "This item is halal."
             return redirect(url_for("result", message=message))
     else:
-        message = "Error! Please try again!" 
+        message = "Error! Please try again!"
         return redirect(url_for("result", message=message))
+
 
 @app.route("/result/<string:message>")
 def result(message: str):
-    return "%s" % message 
+    return "%s" % message
+
 
 if __name__ == "__main__":
     app.run(debug=True)
