475f8668d34f7bf7be237b5d70cc3643bbb56068
Rezwan Matin <rezwanmatin@gmail.com>
2025-06-19 11:14:26 -0400

    Added HTML and style sheets for displaying result

--- flaskr/web-app.py
+++ flaskr/web-app.py
@@ -2,14 +2,13 @@ import json
 import os
 import requests
 from dotenv import load_dotenv
-from flask import Flask, redirect, url_for, request
+from flask import Flask, redirect, url_for, request, render_template
 
 
 app = Flask(__name__)
 
-
 @app.route("/", methods=["POST", "GET"])
-def check_halal():
+def index():
     load_dotenv()
     api_key = os.environ.get("USDA_API_KEY")
     url = "https://api.nal.usda.gov/fdc/v1/foods/search"
@@ -50,31 +49,29 @@ def check_halal():
     )
     if request.method == "POST":
         query = request.form["nm"]
-    else:
-        query = request.args.get("nm")
-    params = {"query": query, "dataType": ["Branded"], "api_key": api_key}
-    response = requests.get(url, params=params)
-    if response.status_code == 200:
-        for item in response.json()["foods"]:
-            ingredients_list = item["ingredients"].split(",")
-            for ingredient in ingredients_list:
-                ingredients.add(ingredient.strip().lower())
-                if ingredient.strip().lower() in haram_ingredients:
-                    haram_list.add(ingredient.strip().lower())
-        if haram_list:
-            message = f"This item might not be halal as it contains: {haram_list}"
-            return redirect(url_for("result", message=message))
+        params = {"query": query, "dataType": ["Branded"], "api_key": api_key}
+        response = requests.get(url, params=params)
+        if response.status_code == 200:
+            for item in response.json()["foods"]:
+                ingredients_list = item["ingredients"].split(",")
+                for ingredient in ingredients_list:
+                    ingredients.add(ingredient.strip().lower())
+                    if ingredient.strip().lower() in haram_ingredients:
+                        haram_list.add(ingredient.strip().lower())
+            if haram_list:
+                message = f"The item might not be halal as it contains: {haram_list}"
+                return redirect(url_for("result", message=message))
+            else:
+                message = "The item is halal."
+                return redirect(url_for("result", message=message))
         else:
-            message = "This item is halal."
+            message = "Error! Please try again!"
             return redirect(url_for("result", message=message))
-    else:
-        message = "Error! Please try again!"
-        return redirect(url_for("result", message=message))
-
+    return render_template("index.html")
 
 @app.route("/result/<string:message>")
 def result(message: str):
-    return "%s" % message
+    return render_template("result.html", message=message)
 
 
 if __name__ == "__main__":
