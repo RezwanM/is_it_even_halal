4ff196572fbf17cae25b49ffd3cd48a606a0d5a7
Rezwan Matin <rezwanmatin@gmail.com>
2025-07-17 15:13:52 -0400

    Added language support for Arabic and Bengali

--- flaskr/haram_info.py
+++ flaskr/haram_info.py
@@ -1,33 +1,47 @@
-info = {
-    "enzymes": "If the enzyme is microbial or plant-based, it is considered halal. If it is animal-based, like rennet, it is only halal provided that the animal itself is halal and the way it was slaughtered is halal.",
-    "whey": "Rennet, the enzyme used to make whey, can come from an animal which is either not halal or was not slaughtered in a halal way.",
-    "lard": "Lard is a fat derived from pigs, which are haram.",
-    "meat": "If the meat comes from an animal which is not halal or which was not slaughtered in a halal way, then it is considered haram to consume that meat.",
-    "bacon": "Bacon is made from pork, which is haram.",
-    "pepperoni": "Pepperoni is almost always made out of pork, which is haram. If it is made from beef, then it is only halal provided that the way the cow was slaughtered is halal.",
-    "pork": "Pork is haram.",
-    "ham": "Ham is made from pork, which is haram.",
-    "chicken": "If the chicken was not slaughtered in a halal way, then it is considered haram to consume its meat.",
-    "beef": "If the cow was not slaughtered in a halal way, then it is considered haram to consume its meat.",
-    "lamb": "If the lamb was not slaughtered in a halal way, then it is considered haram to consume its meat.",
-    "goat": "If the goat was not slaughtered in a halal way, then it is considered haram to consume its meat.",
-    "gelatin": "If this protein comes from an animal which is not halal or which was not slaughtered in a halal way, then it is considered haram.",
-    "rennet": "If this enzyme is derived from an animal which is either not halal or was not slaughtered in a halal way, then it is considered haram.",
-    "l-cysteine": "If this amino acid is sourced from fish, microbial, or plant-based materials, it is considered halal. If it is animal-based, it is only halal provided that the animal itself is halal and the way it was slaughtered is halal.",
-    "pepsin": "If this enzyme is derived from an animal which is either not halal or was not slaughtered in a halal way, then it is considered haram.",
-    "alcohol": "Alcohol is haram.",
-    "ethanol": "Ethanol is an intoxicant, which makes it haram.",
-    "vanilla extract": "Since vanilla extract contains alcohol, it is considered haram.",
-    "wine vinegar": "Wine vinegar is made by fermenting wine, which is haram.",
-    "malt extract": "Due to its alcohol association and use of enzymes which might come from non-halal sources, it is mostly considered haram.",
-    "monoglycerides": "If this emulsifier is derived from an animal which is either not halal or was not slaughtered in a halal way, then it is considered haram.",
-    "diglycerides": "If this emulsifier is derived from an animal which is either not halal or was not slaughtered in a halal way, then it is considered haram.",
-    "e471": "If the E471 (i.e., monoglyceride/diglyceride) emulsifier is derived from an animal which is either not halal or was not slaughtered in a halal way, then it is considered haram.",
-    "glycerin": "If this syrupy liquid is derived from an animal which is either not halal or was not slaughtered in a halal way, then it is considered haram.",
-    "glycerol": "If this syrupy liquid is derived from an animal which is either not halal or was not slaughtered in a halal way, then it is considered haram.",
-    "e422": "If this syrupy liquid (i.e., glycerol) is derived from an animal which is either not halal or was not slaughtered in a halal way, then it is considered haram.",
-    "shortening": "If this fat is derived from an animal which is either not halal or was not slaughtered in a halal way, then it is considered haram.",
-    "magensium stearate": "If the stearic acid is derived from an animal which is either not halal or was not slaughtered in a halal way, then it is considered haram.",
-    "carmine": "Carmine is a red pigment extracted from cochineal insect, which is considered impure or filthy, and therefore haram.",
-    "e120": "E120 (i.e., carmine) is a red pigment extracted from cochineal insect, which is considered impure or filthy, and therefore haram.",
-}
+from deep_translator import GoogleTranslator
+from typing import Dict
+
+
+class Info:
+    def __init__(self):
+        self.translated_info = {}
+        self.info = {
+            "enzymes": "If the enzyme is microbial or plant-based, it is considered halal. If it is animal-based, like rennet, it is only halal provided that the animal itself is halal and the way it was slaughtered is halal.",
+            "whey": "Rennet, the enzyme used to make whey, can come from an animal which is either not halal or was not slaughtered in a halal way.",
+            "lard": "Lard is a fat derived from pigs, which are haram.",
+            "meat": "If the meat comes from an animal which is not halal or which was not slaughtered in a halal way, then it is considered haram to consume that meat.",
+            "bacon": "Bacon is made from pork, which is haram.",
+            "pepperoni": "Pepperoni is almost always made out of pork, which is haram. If it is made from beef, then it is only halal provided that the way the cow was slaughtered is halal.",
+            "pork": "Pork is haram.",
+            "ham": "Ham is made from pork, which is haram.",
+            "chicken": "If the chicken was not slaughtered in a halal way, then it is considered haram to consume its meat.",
+            "beef": "If the cow was not slaughtered in a halal way, then it is considered haram to consume its meat.",
+            "lamb": "If the lamb was not slaughtered in a halal way, then it is considered haram to consume its meat.",
+            "goat": "If the goat was not slaughtered in a halal way, then it is considered haram to consume its meat.",
+            "gelatin": "If this protein comes from an animal which is not halal or which was not slaughtered in a halal way, then it is considered haram.",
+            "rennet": "If this enzyme is derived from an animal which is either not halal or was not slaughtered in a halal way, then it is considered haram.",
+            "l-cysteine": "If this amino acid is sourced from fish, microbial, or plant-based materials, it is considered halal. If it is animal-based, it is only halal provided that the animal itself is halal and the way it was slaughtered is halal.",
+            "pepsin": "If this enzyme is derived from an animal which is either not halal or was not slaughtered in a halal way, then it is considered haram.",
+            "alcohol": "Alcohol is haram.",
+            "ethanol": "Ethanol is an intoxicant, which makes it haram.",
+            "vanilla extract": "Since vanilla extract contains alcohol, it is considered haram.",
+            "wine vinegar": "Wine vinegar is made by fermenting wine, which is haram.",
+            "malt extract": "Due to its alcohol association and use of enzymes which might come from non-halal sources, it is mostly considered haram.",
+            "monoglycerides": "If this emulsifier is derived from an animal which is either not halal or was not slaughtered in a halal way, then it is considered haram.",
+            "diglycerides": "If this emulsifier is derived from an animal which is either not halal or was not slaughtered in a halal way, then it is considered haram.",
+            "e471": "If the E471 (i.e., monoglyceride/diglyceride) emulsifier is derived from an animal which is either not halal or was not slaughtered in a halal way, then it is considered haram.",
+            "glycerin": "If this syrupy liquid is derived from an animal which is either not halal or was not slaughtered in a halal way, then it is considered haram.",
+            "glycerol": "If this syrupy liquid is derived from an animal which is either not halal or was not slaughtered in a halal way, then it is considered haram.",
+            "e422": "If this syrupy liquid (i.e., glycerol) is derived from an animal which is either not halal or was not slaughtered in a halal way, then it is considered haram.",
+            "shortening": "If this fat is derived from an animal which is either not halal or was not slaughtered in a halal way, then it is considered haram.",
+            "magensium stearate": "If the stearic acid is derived from an animal which is either not halal or was not slaughtered in a halal way, then it is considered haram.",
+            "carmine": "Carmine is a red pigment extracted from cochineal insect, which is considered impure or filthy, and therefore haram.",
+            "e120": "E120 (i.e., carmine) is a red pigment extracted from cochineal insect, which is considered impure or filthy, and therefore haram.",
+        }
+
+    def get_info(self, language: str) -> Dict[str, str]: 
+        for key, val in self.info.items():
+            translated_key = GoogleTranslator(source='auto', target=language).translate(text=key)
+            translated_val = GoogleTranslator(source='auto', target=language).translate(text=val)
+            self.translated_info[translated_key] = translated_val
+        return self.translated_info
--- flaskr/web-app.py
+++ flaskr/web-app.py
@@ -1,14 +1,15 @@
 import json
 import os
 import requests
+from deep_translator import GoogleTranslator
 from dotenv import load_dotenv
 from flask import Flask, redirect, url_for, request, render_template
 from typing import Set
 
-import haram_info
+from haram_info import Info
+
 
 app = Flask(__name__)
-
 
 @app.route("/", methods=["POST", "GET"])
 def base():
@@ -63,6 +64,9 @@ def base():
         + insects_derived_ingredients
     )
     if request.method == "POST":
+        lang = request.form["lang"]
+        if not lang:
+            lang = "english"
         query = request.form["nm"]
         params = {
             "query": query,
@@ -91,22 +95,23 @@ def base():
                 message += "The item is halal."
         else:
             message = "Error! Please try again!"
-        return redirect(url_for("result", message=message, haram_list=haram_list))
+        return redirect(url_for("result", message=GoogleTranslator(source='auto', target=lang).translate(text=message), haram_list=haram_list, language=lang))
     return render_template("base.html")
 
 
-@app.route("/result/<string:message>/<string:haram_list>")
-def result(message: str, haram_list: Set[str]):
+@app.route("/result/<string:message>/<string:haram_list>/<string:language>")
+def result(message: str, haram_list: Set[str], language: str):
     formatted_message = message.replace("\n", "<br>")
     haram_string = haram_list.lstrip("{").rstrip("}")
     stripped_list = haram_string.split(",")
     for i in range(len(stripped_list)):
         stripped_list[i] = stripped_list[i].strip(" ").strip("'").strip("set()")
+        stripped_list[i] = GoogleTranslator(source='auto', target=language).translate(text=stripped_list[i])
     return render_template(
         "result.html",
         message=formatted_message,
         haram_list=stripped_list,
-        haram_dict=haram_info.info,
+        haram_dict=Info().get_info(language=language),
     )
 
 
